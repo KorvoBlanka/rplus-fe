72b91ba52207d848be49b19d166e315568d06ce2
KorvoBlanka <korvo.blanka@gmail.com>
2017-02-06 14:29:48 +1000

    beta1 fe c8 удалил js и js.map

--- app/class/organisation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Organisation = (function () {
-    function Organisation() {
-    }
-    return Organisation;
-}());
-exports.Organisation = Organisation;
-//# sourceMappingURL=organisation.js.map

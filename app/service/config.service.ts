aa1fa391e16a809ee297219b2140945ad5137add
KorvoBlanka <korvo.blanka@gmail.com>
2017-03-22 18:40:37 +1000

    beta1 fe c16.7

--- app/service/config.service.ts
+++ app/service/config.service.ts
@@ -11,7 +11,7 @@ export class ConfigService {
 }
 
 var CONFIG: Config = {
-    version: "b16.5",
+    version: "b16.7",
     map: {
         default: {
             lat: 55.755505, lon: 37.617270, zoom: 14
@@ -27,6 +27,21 @@ var CONFIG: Config = {
         },
         vld: {
             lat: 43.119348, lon: 131.886965, zoom: 14
+        },
+        irkutsk: {
+            lat: 52.286450, lon: 104.304856, zoom: 14
+        },
+        tver: {
+            lat: 56.858507, lon: 35.917521, zoom: 14
+        },
+        rzn: {
+            lat: 54.609215, lon: 39.712564, zoom: 14
+        },
+        kms: {
+            lat: 50.546913, lon: 137.011227, zoom: 14
+        },
+        nahodka: {
+            lat: 42.820431, lon: 132.883842, zoom: 14
         }
     },
     RESTServer: "http://maklersoft.com:4567",

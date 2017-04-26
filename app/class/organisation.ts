6e2b206c64e9e94b5772b936e4d8ac49818475bd
KorvoBlanka <korvo.blanka@gmail.com>
2017-04-26 21:05:03 +1000

    beta1 fe c16.952

--- app/class/organisation.ts
+++ app/class/organisation.ts
@@ -11,4 +11,64 @@ export class Organisation {
     addDate: number;
 
     changeDate: number;
+
+
+    typeCode_n: string;
+
+    orgName_n: string;
+
+    region_n: string;
+
+    city_n: string;
+
+    area_n: string;
+
+    admArea_n: string;
+
+    street_n: string;
+
+    house_n: string;
+
+    housing_n: string;
+
+    apartment_n: string;
+
+    officePhone_n: string;
+
+    homePhone_n: string;
+
+    cellPhone_n: string;
+
+    fax_n: string;
+
+    mainPhone_n: string;
+
+    otherPhone_n: string;
+
+    workEmail_n: string;
+
+    mainEmail_n: string;
+
+    webSite_n: string;
+
+    inn_n: string;
+
+    kpp_n: string;
+
+    cor_n: string;
+
+    bic_n: string;
+
+    other_n: string;
+
+    head_n: any;
+
+    contact_n: any;
+
+
+    contract_n: string;
+
+    stateCode_n: string;
+
+    sourceCode_n: string;
 }
